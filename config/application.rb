0f882db6d736884896609c354abc9c1e1a803c1f
rafasilverio <rafasilverio0@gmail.com>
2013-12-07 20:04:11 -0200

    install devise, add a user, and install devise views

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(:default, Rails.env)
 module Treebook
   class Application < Rails::Application
   	config.assets.paths << "#{Rails}/vendor/assets/fonts"
+  	config.assets.initialize_on_precompile = false
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
